4a1ff3a00f4f896ec1d37e9112d29d9ef0159a1d
Yves Richard <yves.richard@tesera.com>
2016-08-09 16:47:44 -0600

    WIP
    Added to share with MS PBI team for insight on how to build selection ids.

--- src/visual.ts
+++ src/visual.ts
@@ -5,12 +5,16 @@ module powerbi.extensibility.visual {
     $head.append('<script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.5/leaflet.js"></script>');
 
     export class LeafletMap implements IVisual {
+        private selectionIdBuilder: ISelectionIdBuilder;
+        private selectionManager: ISelectionManager;
         private dataView: DataView;
         private map: L.Map;
         private basemap: L.TileLayer;
 
         constructor(options: VisualConstructorOptions) {
             console.log('constructor called');
+            this.selectionIdBuilder = options.host.createSelectionIdBuilder();
+            this.selectionManager = options.host.createSelectionManager();
             let $mapDiv = $('<div></div>')
                 .attr('id', 'map')
                 .css('height', 500)
@@ -34,12 +38,16 @@ module powerbi.extensibility.visual {
             const {columns, rows} = dataView.table;
             const c20 = d3.scale.category10();
 
-            const datas = rows.map(function (row) {
+            const datas = rows.map(function (row, idx) {
                 let data = row.reduce(function (d, v, i) {
                     d[columns[i].displayName] = v;
                     return d;
                 }, {});
 
+                data.identity = this.selectionIdBuilder()
+                    .withSeries(rows, rows[idx])
+                    .createSelectionId();
+                
                 data.color = c20(data.category);
 
                 return data;
@@ -57,7 +65,16 @@ module powerbi.extensibility.visual {
 
             const markers = data.map(function (d) {
                 const latlng = L.latLng([d.latitude, d.longitude]);
-                return L.circleMarker(latlng, {color: d.color, fillOpacity: 1});
+                let marker = L.circleMarker(latlng, {color: d.color, fillOpacity: 1});
+
+                marker.on('click', function (evt) {
+                    console.log('marker identity is ', d.identity)
+                    this.selectionManager.select(d.identity);
+                    // .then((ids: ISelectionId[]) => {
+                    //     //called when setting the selection has been completed successfully
+                    // });
+                });
+                return marker;
             });
 
             const markerLayer = L.layerGroup(markers);
